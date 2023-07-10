11c855f3d33af3c1356ebb0563b4226914a14373
firhan abigael <firhangael1525@gmail.com>
2023-07-11 03:01:45 +0700

    add new app config

--- app/configs/app.config.ts
+++ app/configs/app.config.ts
@@ -1,6 +1,9 @@
+type appMode = "development" | "production";
+
 interface appConfig {
   APP_NAME: string;
   APP_PORT: number;
+  APP_MODE: appMode;
   DB_HOST: string;
   DB_PORT: number;
   DB_NAME: string;
@@ -9,21 +12,23 @@ interface appConfig {
   DB_DIALECT: string;
   JWT_SECRET: string;
   JWT_EXPIRED: string;
+  LOG_NAME: string;
 }
 
 /**
- * 
+ *
  */
 export const AppConfig: appConfig = {
-    APP_NAME: String(process.env.APP_NAME),
-    APP_PORT: parseInt(process.env.APP_PORT ?? "0"),
-    DB_HOST: String(process.env.DB_HOST),
-    DB_PORT: 0,
-    DB_NAME: String(process.env.DB_NAME),
-    DB_USER: String(process.env.DB_USER),
-    DB_PASS: String(process.env.DB_PASS),
-    DB_DIALECT: String(process.env.DB_DIALECT),
-    JWT_SECRET: String(process.env.JWT_SECRET),
-    JWT_EXPIRED: String(process.env.JWT_EXPIRED)
-}
-
+  APP_NAME: String(process.env.APP_NAME),
+  APP_PORT: parseInt(process.env.APP_PORT ?? "0"),
+  APP_MODE: String(process.env.APP_MODE) as appMode,
+  DB_HOST: String(process.env.DB_HOST),
+  DB_PORT: 0,
+  DB_NAME: String(process.env.DB_NAME),
+  DB_USER: String(process.env.DB_USER),
+  DB_PASS: String(process.env.DB_PASS),
+  DB_DIALECT: String(process.env.DB_DIALECT),
+  JWT_SECRET: String(process.env.JWT_SECRET),
+  JWT_EXPIRED: String(process.env.JWT_EXPIRED),
+  LOG_NAME: String(process.env.LOG_NAME),
+};
