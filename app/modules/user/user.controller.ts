0ea412a93d672b00b506a0ec58cab1b585fa5df3
firhan abigael <firhangael1525@gmail.com>
2023-07-18 02:43:48 +0700

    remove unused

--- app/modules/user/user.controller.ts
+++ app/modules/user/user.controller.ts
@@ -3,38 +3,18 @@ import { UserService } from "./user.service";
 import { JwtMiddleware } from "../../middlewares/jwt.middleware";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { schema, schemaCompile } from "./schemas/user-create.schema";
+import { sendEmail } from "../../utils/email.util";
 
 @Controller({ route: "/user" })
 export default class UserController {
   @Inject(UserService)
   private userService!: UserService;
 
-  @POST({
-    url: "/save",
-    options: {
-      config: {
-        rateLimit: {
-          max: 3,
-          timeWindow: "30 seconds",
-        },
-      },
-      validatorCompiler: () => {
-        return schemaCompile;
-      },
-      schema: {
-        body: schema,
-      },
-    },
-  })
-  async helloHandler(request: FastifyRequest, reply: FastifyReply) {
-    const token = await reply.jwtSign({ foo: "bar" });
-    return reply.code(200).send({
-      token,
-    });
-  }
-
   @GET({
     url: "/list",
+    options: {
+      onRequest: [JwtMiddleware]
+    }
   })
   async list() {
     return await this.userService.getUsers();
