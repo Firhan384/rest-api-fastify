e495891e0e880207b51ff29e065c75b4a8e087f1
firhan abigael <firhangael1525@gmail.com>
2023-07-18 02:35:56 +0700

    register auth controller

--- app/module.ts
+++ app/module.ts
@@ -1,7 +1,8 @@
 // the module has function as a wrapper controllers
 import { Constructor } from "fastify-decorators/decorators/helpers/inject-dependencies";
 import UserController from "./modules/user/user.controller";
+import AuthController from "./modules/auth/auth.controller";
 
 export const controllersModule: { controllers: Constructor<unknown>[] } = {
-  controllers: [UserController],
+  controllers: [UserController, AuthController],
 };
